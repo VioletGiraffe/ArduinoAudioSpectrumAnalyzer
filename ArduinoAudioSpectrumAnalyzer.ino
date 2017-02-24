38b2150e7be1db78c91016a2993af8e79596f483
VioletGiraffe <noreply@bitbucket.org>
2017-02-25 00:34:35 +0200

    FHT library added; hi-speed ADC setup

--- ArduinoAudioSpectrumAnalyzer.ino
+++ ArduinoAudioSpectrumAnalyzer.ino
@@ -15,6 +15,13 @@
 #include <algorithm>
 #include <math.h>
 
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif 
+
 #define analogInPin A0
 
 uint16_t sample = 0, maxSampleValue = 0;
@@ -25,9 +32,12 @@ Adafruit_ST7735 tft = Adafruit_ST7735(ST7735_CS_PIN,  ST7735_DC_PIN, TFT_RST);
 PDQ_ST7735 tft;
 #endif
 
-void setup() {
+void setup()
+{
 //  Serial.begin(9600);
 //  while (!Serial);
+
+  setupADC();
 
   // Use this initializer (uncomment) if you're using a 1.44" TFT
   tft.initR(ST7735_INITR_144GREENTAB);   // initialize a ST7735S chip, black tab
@@ -37,6 +47,26 @@ void setup() {
 
   Timer1.initialize(100L * 1000L); // initialize timer1, 1/30th sec period
   Timer1.attachInterrupt(updateScreen);
+}
+
+void setupADC()
+{
+  // set a2d prescaled factor to 32
+  // 16 MHz / 32 = 500 KHz, for sampling rate of 500 000 / 13 = 38461.54 Hz
+  // XXX: this will not work properly for other clock speeds, and
+  // this code should use F_CPU to determine the prescaled factor.
+
+  // See http://garretlab.web.fc2.com/en/arduino/inside/arduino/wiring_analog.c/analogRead.html for how prescaling is encoded. Basically, it's a list of factors from 1 to 128
+  // 32 is 6th in the list, so it's 101
+  
+  sbi(ADCSRA, ADPS2);
+  cbi(ADCSRA, ADPS1);
+  sbi(ADCSRA, ADPS0);
+
+  // enable a2d conversions
+  sbi(ADCSRA, ADEN);
+
+  analogReference(DEFAULT); // Use default (5v) aref voltage.
 }
 
 template <typename T> void printNumber(T number, uint16_t color, bool newLineAfter = false)
@@ -56,7 +86,8 @@ inline uint16_t RGB888_to_565(uint8_t R, uint8_t G, uint8_t B)
     (((B >> 3) & 0x1f)      );
 }
 
-void loop() {
+void loop()
+{
   // read the analog in value:
   sample = analogRead(analogInPin);
   maxSampleValue = std::max(maxSampleValue, sample);
