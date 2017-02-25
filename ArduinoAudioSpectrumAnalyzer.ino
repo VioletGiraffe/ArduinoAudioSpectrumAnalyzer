9f1cfb8726aea887d98e402a137cf6c421bfd446
VioletGiraffe <noreply@bitbucket.org>
2017-02-26 00:48:41 +0200

    Advanced ADC setup; sample buffer; timer removed

--- ArduinoAudioSpectrumAnalyzer.ino
+++ ArduinoAudioSpectrumAnalyzer.ino
@@ -15,16 +15,7 @@
 #include <algorithm>
 #include <math.h>
 
-#ifndef cbi
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#endif
-#ifndef sbi
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
-#endif 
-
-#define analogInPin A0
-
-uint16_t sample = 0, maxSampleValue = 0;
+#include "utils.h"
 
 #ifndef _PDQ_ST7735H_
 Adafruit_ST7735 tft = Adafruit_ST7735(ST7735_CS_PIN,  ST7735_DC_PIN, TFT_RST);
@@ -34,9 +25,6 @@ PDQ_ST7735 tft;
 
 void setup()
 {
-//  Serial.begin(9600);
-//  while (!Serial);
-
   setupADC();
 
   // Use this initializer (uncomment) if you're using a 1.44" TFT
@@ -44,13 +32,15 @@ void setup()
 
   tft.setTextSize(3);
   tft.fillScreen(ST7735_BLACK);
-
-  Timer1.initialize(100L * 1000L); // initialize timer1, 1/30th sec period
-  Timer1.attachInterrupt(updateScreen);
 }
 
 void setupADC()
 {
+  cli();
+  
+  ADCSRA = 0;
+  ADCSRB = 0;
+  
   // set a2d prescaled factor to 32
   // 16 MHz / 32 = 500 KHz, for sampling rate of 500 000 / 13 = 38461.54 Hz
   // XXX: this will not work properly for other clock speeds, and
@@ -58,15 +48,59 @@ void setupADC()
 
   // See http://garretlab.web.fc2.com/en/arduino/inside/arduino/wiring_analog.c/analogRead.html for how prescaling is encoded. Basically, it's a list of factors from 1 to 128
   // 32 is 6th in the list, so it's 101
-  
-  sbi(ADCSRA, ADPS2);
-  cbi(ADCSRA, ADPS1);
-  sbi(ADCSRA, ADPS0);
+  setBit(ADCSRA, ADPS2);
+  clearBit(ADCSRA, ADPS1);
+  setBit(ADCSRA, ADPS0);
 
-  // enable a2d conversions
-  sbi(ADCSRA, ADEN);
+  setBit(ADCSRA, ADATE); //enabble auto trigger
+  setBit(ADCSRA, ADEN); //enable ADC
+  setBit(ADCSRA, ADSC); //start ADC measurements
 
   analogReference(DEFAULT); // Use default (5v) aref voltage.
+
+  sei();
+}
+
+uint16_t maxSampleValue = 0;
+uint16_t minSampleValue = 65535;
+
+constexpr uint16_t SamplingWindowSize = 256;
+
+volatile bool samplingWindowFull = false;
+uint16_t currentSampleIndex = 0;
+uint16_t samples[SamplingWindowSize] = {0};
+
+
+ISR(ADC_vect) //when new ADC value ready
+{
+  if (samplingWindowFull)
+    return;
+  
+  const uint16_t sample = ADCH;
+  samples[currentSampleIndex] = sample;
+  ++currentSampleIndex;
+
+  if (currentSampleIndex == SamplingWindowSize)
+  {
+    currentSampleIndex = 0;
+    samplingWindowFull = true;
+  }
+  
+  if (sample > maxSampleValue)
+    maxSampleValue = sample;
+  else if (sample < minSampleValue)
+    minSampleValue = sample;      
+}
+
+void loop()
+{
+  if (samplingWindowFull)
+  {
+    updateScreen();
+    delay(60);
+    samplingWindowFull = false;
+  }
+    
 }
 
 template <typename T> void printNumber(T number, uint16_t color, bool newLineAfter = false)
@@ -78,23 +112,6 @@ template <typename T> void printNumber(T number, uint16_t color, bool newLineAft
     tft.print(number);  
 }
 
-inline uint16_t RGB888_to_565(uint8_t R, uint8_t G, uint8_t B)
-{
-  return
-    (((R >> 3) & 0x1f) << 11) |
-    (((G >> 2) & 0x3f) <<  6) |
-    (((B >> 3) & 0x1f)      );
-}
-
-void loop()
-{
-  // read the analog in value:
-  sample = analogRead(analogInPin);
-  maxSampleValue = std::max(maxSampleValue, sample);
-
-  delay(1);
-}
-
 void updateScreen()
 {
   tft.fillRect(0, 0, 100, 40, ST7735_BLACK);
@@ -104,11 +121,10 @@ void updateScreen()
   
   tft.setTextSize(3);
   tft.setCursor(0, 0);
-  printNumber(sample, textColor1);
+  printNumber(ADCH, textColor1);
 
   tft.setTextSize(2);
   tft.setCursor(0, 25);
   tft.print("Max: ");
   printNumber(maxSampleValue, textColor2);
 }
-
---
+++ utils.h
@@ -0,0 +1,14 @@
+#ifndef clearBit
+#define clearBit(register, bit) register &= ~(1 << bit)
+#endif
+#ifndef setBit
+#define setBit(register, bit) register |= (1 << bit)
+#endif 
+
+inline uint16_t RGB888_to_565(uint8_t R, uint8_t G, uint8_t B)
+{
+  return
+    (((R >> 3) & 0x1f) << 11) |
+    (((G >> 2) & 0x3f) <<  6) |
+    (((B >> 3) & 0x1f)      );
+}
